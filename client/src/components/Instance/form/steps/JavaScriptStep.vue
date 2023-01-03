618469c2cf692a5a2b786cd6cf13c6e5866a5420
acelancer-z <acelancer1076@outlook.com>
2023-01-04 01:14:33 +0500

    JavaScript step

--- client/src/components/Instance/form/steps/JavaScriptStep.vue
+++ client/src/components/Instance/form/steps/JavaScriptStep.vue
@@ -1,12 +1,78 @@
 <template>
-  <div>
-    <h2>JavaScript</h2>
-    {{ JSON.stringify(javascript) }}
+  <div class="proxy-step">
+    <div class="section">
+      <a-row class="row">
+        <a-col span="24">
+          <base-input-group
+            name="disableJavascriptHarmonyShipping"
+            label="Disable JavaScript harmony shipping"
+          >
+            <template #afterLabel>
+              <base-checkbox
+                class="checkbox"
+                @change="onChangeField('javascript.disableJavascriptHarmonyShipping', $event.target.checked)"
+                :value="form.javascript.disableJavascriptHarmonyShipping"
+              />
+            </template>
+
+            <template #description>
+              Disable the latest shipping ECMAScript 6 features.
+            </template>
+          </base-input-group>
+        </a-col>
+      </a-row>
+
+      <a-row class="row">
+        <a-col span="24">
+          <base-input-group
+            name="javascriptHarmony"
+            label="JavaScript harmony"
+          >
+            <template #afterLabel>
+              <base-checkbox
+                class="checkbox"
+                @change="onChangeField('javascript.javascriptHarmony', $event.target.checked)"
+                :value="form.javascript.javascriptHarmony"
+              />
+            </template>
+
+            <template #description>
+              Enables experimental Harmony (ECMAScript 6) features
+            </template>
+          </base-input-group>
+        </a-col>
+      </a-row>
+
+      <a-row class="row">
+        <a-col span="24">
+          <base-input-group
+            name="jsFlags"
+            label="JavaScript flags"
+          >
+            <base-input
+              @change="onChangeField('javascript.jsFlags', $event.target.value)"
+              :value="form.javascript.jsFlags"
+            />
+
+            <template #description>
+              Specifies the flags passed to JS engine.
+            </template>
+          </base-input-group>
+        </a-col>
+      </a-row>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { storeToRefs } from 'pinia'
 import { useNewInstanceFormStore } from '~/stores/newInstanceFormStore.js'
 
-const { form: { javascript } } = useNewInstanceFormStore()
+import BaseCheckbox from '~/components/Base/Form/BaseCheckbox.vue'
+import BaseInput from '~/components/Base/Form/BaseInput.vue'
+import BaseInputGroup from '~/components/Base/Form/BaseInputGroup.vue'
+
+const store = useNewInstanceFormStore()
+const { onChangeField } = store
+const { form } = storeToRefs(store)
 </script>
