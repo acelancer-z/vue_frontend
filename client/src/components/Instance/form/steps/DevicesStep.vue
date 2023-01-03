a2321daf98d4c8ea60ec376ab858ea3cc65c6dbe
acelancer-z <acelancer1076@outlook.com>
2023-01-04 01:50:51 +0500

    Devices step

--- client/src/components/Instance/form/steps/DevicesStep.vue
+++ client/src/components/Instance/form/steps/DevicesStep.vue
@@ -1,12 +1,57 @@
 <template>
-  <div>
-    <h2>Devices</h2>
-    {{ JSON.stringify(devices) }}
+  <div class="devices-step">
+    <div class="section">
+      <a-row class="row">
+        <a-col span="24">
+          <base-input-group
+            name="qsAddFakeBluetoothDevices"
+            label="Qs add fake bluetooth devices"
+          >
+            <template #default>
+              <base-input
+                @change="onChangeField('devices.qsAddFakeBluetoothDevices', $event.target.value)"
+                :value="form.devices.qsAddFakeBluetoothDevices"
+              />
+            </template>
+
+            <template #description>
+              Adds fake Bluetooth devices to the quick settings menu.
+            </template>
+          </base-input-group>
+        </a-col>
+      </a-row>
+
+      <a-row class="row">
+        <a-col span="24">
+          <base-input-group
+            name="qsAddFakeCastDevices"
+            label="Qs add fake cast devices"
+          >
+            <template #default>
+              <base-input
+                @change="onChangeField('devices.qsAddFakeCastDevices', $event.target.value)"
+                :value="form.devices.qsAddFakeCastDevices"
+              />
+            </template>
+
+            <template #description>
+              Adds fake Cast devices to the quick settings menu
+            </template>
+          </base-input-group>
+        </a-col>
+      </a-row>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { storeToRefs } from 'pinia'
 import { useNewInstanceFormStore } from '~/stores/newInstanceFormStore.js'
 
-const { form: { devices } } = useNewInstanceFormStore()
+import BaseInput from '~/components/Base/Form/BaseInput.vue'
+import BaseInputGroup from '~/components/Base/Form/BaseInputGroup.vue'
+
+const store = useNewInstanceFormStore()
+const { onChangeField } = store
+const { form } = storeToRefs(store)
 </script>
