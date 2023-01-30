1b605c11287624942f9919cce658317e2f233a6f
acelancer-z <acelancer1076@outlook.com>
2023-01-30 19:51:19 +0500

    Added browser select version

--- client/src/const/browser.js
+++ client/src/const/browser.js
@@ -6,8 +6,24 @@ export const BROWSERS = [
 ];
 
 export const CHROME_VERSIONS = [
+  /*{
+    label: '109',
+    value: 109
+  },*/
   {
     label: '108',
     value: 108
+  },
+  {
+    label: '107',
+    value: 107
+  },
+  {
+    label: '106',
+    value: 106
+  },
+  {
+    label: '105',
+    value: 105
   }
 ];
