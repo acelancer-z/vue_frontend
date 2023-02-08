f6e4a6f9e4be5a3b83a1a90da8ec574dd68cac14
acelancer-z <acelancer1076@outlook.com>
2023-02-09 04:08:07 +0500

    Fixed download links 2

--- client/src/helpers/download.js
+++ client/src/helpers/download.js
@@ -4,18 +4,18 @@ if (!baseURL) {
 }
 
 export const DOWNLOAD_LINKS = {
-    windows: 'https://drive.google.com/uc?export=download&id=1mJa8kwwEKq0OUvgaM4gA8ttf1_fCW6BG',
-    macosIntel: 'https://drive.google.com/uc?export=download&id=1hiEu_OeDAjf2-v9r6bqr4b3lTpTquPAI',
-    macosApple: 'https://drive.google.com/uc?export=download&id=1y0ua4GACrIeUnqNruYT320DUWIic5MKc',
+    windows: 'browser-profiles-windows.exe',
+    macosIntel: 'browser-profiles-mac_intel.dmg.zip',
+    macosApple: 'browser-profiles-mac_apple.zip',
     linux: 'download/linux',
 }
 
-export const LATEST_VERSION = '1.0'
+export const LATEST_VERSION = '100'
 
 export const downloadFile = (system, version = LATEST_VERSION) => {
     if (!DOWNLOAD_LINKS[system]) {
         console.error('Wrong download system', system)
     }
 
-    window.open(DOWNLOAD_LINKS[system], '_blank')
+    window.open(`${baseURL}/${version}/${DOWNLOAD_LINKS[system]}`, '_blank')
 }
