b9b0f6953bfc95e379e227e91cd0064fd6b273f8
acelancer-z <acelancer1076@outlook.com>
2023-02-10 15:20:05 +0500

    Added hook redirect & message

---
+++ client/src/pages/Payment/PaymentStatus.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page">
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import { useToast } from 'vue-toastification'
+
+const router = useRouter()
+const route = useRoute()
+const toast = useToast()
+
+const { t } = i18n
+
+const loading = ref(false)
+
+onMounted(() => {
+  const type = route.query.type ?? 'fail'
+  if (type === 'success') {
+    toast.success(t('messages.success.payment'))
+  } else {
+    toast.error(t('messages.error.payment'))
+  }
+  router.push('/cabinet')
+})
+</script>
