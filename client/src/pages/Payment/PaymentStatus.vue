f8d34d85452cd20652e9d76345853319f6f754be
acelancer-z <acelancer1076@outlook.com>
2023-02-10 15:24:39 +0500

    Fixed payment status

--- client/src/pages/Payment/PaymentStatus.vue
+++ client/src/pages/Payment/PaymentStatus.vue
@@ -7,10 +7,12 @@
 import { onMounted, ref } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useToast } from 'vue-toastification'
+import { useI18n } from 'vue-i18n'
 
 const router = useRouter()
 const route = useRoute()
 const toast = useToast()
+const i18n = useI18n()
 
 const { t } = i18n
 
