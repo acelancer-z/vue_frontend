11b5fea59e4612afc3e4836bdd9e6f1c2399363c
acelancer-z <acelancer1076@outlook.com>
2023-02-13 12:52:47 +0500

    Fixed download links

--- client/src/helpers/download.js
+++ client/src/helpers/download.js
@@ -5,12 +5,11 @@ if (!baseURL) {
 
 export const DOWNLOAD_LINKS = {
     windows: 'browser-profiles-windows.exe',
-    macosIntel: 'browser-profiles-mac_intel.dmg.zip',
-    macosApple: 'browser-profiles-mac_apple.zip',
+    macosIntel: 'browser-profiles-mac_intel.dmg',
     linux: 'download/linux',
 }
 
-export const LATEST_VERSION = '100'
+export const LATEST_VERSION = '101'
 
 export const downloadFile = (system, version = LATEST_VERSION) => {
     if (!DOWNLOAD_LINKS[system]) {
